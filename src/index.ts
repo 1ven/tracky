fd14a06e663ae734546bc998fd860789248d2697
Ivan Mironchik <me.roaiven@gmail.com>
2017-04-08 22:22:39 +0300

    Init server. Connect to database

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+import * as express from 'express';
+import * as pgp from 'pg-promise';
 
+const app = express();
+const db = pgp()(process.env.DATABASE_URL);
+
+db.any('select * from information_schema.tables').then(console.log).catch(console.log);
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
+
+app.listen(process.env.port, () => {
+  console.log(`Listening at ${process.env.PORT}`);
+});
