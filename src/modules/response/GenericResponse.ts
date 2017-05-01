061cde74e82aaad1453b3c9434c561857098bee5
Ivan Mironchik <me.roaiven@gmail.com>
2017-05-01 15:51:54 +0300

    Add import. Remove comments

--- src/modules/response/GenericResponse.ts
+++ src/modules/response/GenericResponse.ts
@@ -1,8 +1,4 @@
-// 1. Not to repeat { result: ... } every time
-// 2. Make it scalable.
-// - If we will replace JSON by XML in the future, we need to do this in one place.
-// - If we will decide to add { meta: ... } field to any existing response, we need to able to do this by composition, via decorator, not editing existing code.
-// 3. Use one instance for common responses
+import { RsJson, RsWrap, PlainData } from 'chunks';
 
 export class GenericResponse extends RsWrap {
   constructor(data: PlainData) {
