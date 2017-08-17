f220af330361f49610785621113e7ac44d3c52e0
1ven <1ven.mironchik@gmail.com>
2017-08-17 23:25:08 +0300

    Add headers chunk

--- src/core/chunks.ts
+++ src/core/chunks.ts
@@ -1,5 +1,10 @@
-import { makeFork, withStatus, json } from 'chunks';
+import { makeFork, withStatus, json, withHeaders } from 'chunks';
 
 export const fork = makeFork(withStatus(404)(json({
   message: 'Not Found'
 })))
+
+export const headers = (chunk) => async (req) =>
+  withHeaders({
+    "Access-Control-Allow-Origin": "*"
+  })(await chunk(req));
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { compose } from 'ramda';
 import { start, safe, route } from 'chunks';
-import { fork } from './core/chunks';
+import { fork, headers } from './core/chunks';
 import initDatabase from './core/database';
 import modules from './modules';
 
 const db = initDatabase();
-const app = compose(safe)(fork(
+const app = compose(safe, headers)(fork(
   route('/v1*', modules({ db }))
 ));
 
