a6ed642df1a6838f795b27e139d679316f5d329b
1ven <1ven.mironchik@gmail.com>
2017-08-17 22:34:23 +0300

    Pretty code

--- src/modules/entities/tickets/index.ts
+++ src/modules/entities/tickets/index.ts
@@ -1,20 +1,21 @@
 import { compose } from "ramda";
 import { tap, methods, route } from "chunks";
-import { fork } from 'core/chunks';
+import { fork } from "core/chunks";
 import { readAll, read, create, remove, update } from "./controller";
 import init from "./storage";
 
+// prettier-ignore
 export default ({ db }) =>
   compose(tap(() => init(db)))(
     fork(
-      route('/', fork(
+      route("/", fork(
         methods("GET", readAll({ db })),
-        methods('POST', create({ db })),
+        methods("POST", create({ db })),
       )),
-      route('/:id', fork(
-        methods('GET', read({ db })),
-        methods('DELETE', remove({ db })),
-        methods('PATCH', update({ db }))
+      route("/:id", fork(
+        methods("GET", read({ db })),
+        methods("DELETE", remove({ db })),
+        methods("PATCH", update({ db }))
       ))
     )
   );
--- src/modules/entities/tickets/model.ts
+++ src/modules/entities/tickets/model.ts
@@ -1,16 +1,16 @@
-import * as pgp from 'pg-promise';
-import { keys, values } from 'ramda';
-import { Partial } from 'core/types';
+import * as pgp from "pg-promise";
+import { keys, values } from "ramda";
+import { Partial } from "core/types";
 
 export type Ticket = {
   id: number;
   title: string;
-}
+};
 
 export const readAll = db => db.query("SELECT * FROM tickets");
 
 export const create = (title: Ticket["title"], db) =>
-  db.one('INSERT INTO tickets (title) VALUES ($1) RETURNING *', [title]);
+  db.one("INSERT INTO tickets (title) VALUES ($1) RETURNING *", [title]);
 
 export const read = (id: Ticket["id"], db) =>
   db.one("SELECT * FROM tickets WHERE id = $1", [id]);
