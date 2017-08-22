4f18e9d87c532cf25851b375404e039ceba13ffc
1ven <1ven.mironchik@gmail.com>
2017-08-22 19:10:33 +0300

    Using mustache template

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as fs from "fs";
 import * as path from "path";
+import { render } from 'mustache';
 import { compose } from "ramda";
 import { start, safe, route, html } from "chunks";
+import { index } from 'tracky-static';
 import { fork, cors } from "./core/chunks";
 import initDatabase from "./core/database";
 import initModels from "./models";
@@ -13,10 +15,8 @@ export const app = ({ db }) =>
   compose(safe, cors)(
     fork(
       route("/v1*", controllers({ db })),
-      route("/", async () => html(index))
+      route("/", async () => html(render(index)))
     )
   );
 
 initModels(db).then(() => start(app({ db }), parseInt(process.env.PORT)));
-
-const index = fs.readFileSync(path.resolve(__dirname, "index.html"), "utf-8");
