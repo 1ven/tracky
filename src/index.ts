1c145d67e82942566b61426f37bb1fd6f0d3ed1a
1ven <1ven.mironchik@gmail.com>
2017-08-22 17:47:17 +0300

    Returning index.html

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
-import { compose } from 'ramda';
-import { start, safe, route } from 'chunks';
-import { fork, cors } from './core/chunks';
-import initDatabase from './core/database';
-import initModels from './models';
-import controllers from './controllers';
+import * as fs from "fs";
+import * as path from "path";
+import { compose } from "ramda";
+import { start, safe, route, html } from "chunks";
+import { fork, cors } from "./core/chunks";
+import initDatabase from "./core/database";
+import initModels from "./models";
+import controllers from "./controllers";
 
 const db = initDatabase();
 
-export const app = ({ db }) => compose(safe, cors)(fork(
-  route('/v1*', controllers({ db }))
-));
+export const app = ({ db }) =>
+  compose(safe, cors)(
+    fork(
+      route("/v1*", controllers({ db })),
+      route("/", async () => html(index))
+    )
+  );
 
-initModels(db).then(
-  () => start(app({ db }), parseInt(process.env.PORT))
-)
+initModels(db).then(() => start(app({ db }), parseInt(process.env.PORT)));
 
+const index = fs.readFileSync(path.resolve(__dirname, "index.html"), "utf-8");
