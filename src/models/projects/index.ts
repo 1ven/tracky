9828d97ccf3fee0a275bf7f9f7c6f766b2f1d410
1ven <1ven.mironchik@gmail.com>
2017-08-25 11:59:21 +0300

    Add projects_tickets table

--- src/models/projects/index.ts
+++ src/models/projects/index.ts
@@ -5,5 +5,9 @@ export default (db) => db.none(
   'CREATE TABLE IF NOT EXISTS projects(' +
     'id serial PRIMARY KEY,' +
     'name text UNIQUE NOT NULL' +
+  ');' +
+  'CREATE TABLE IF NOT EXISTS projects_tickets(' +
+    'project_id integer REFERENCES projects (id) ON DELETE CASCADE,' +
+    'ticket_id integer UNIQUE REFERENCES tickets (id) ON DELETE CASCADE' +
   ');'
 )
